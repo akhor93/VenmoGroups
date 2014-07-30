e565bb11dc5442055549498f06911537d00f99e0
Andrew Khor <andrew.khow@venmo.com>
2014-07-30 18:00:31 -0400

    members are now saved as a json array of venmo ids

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,16 +2,17 @@ class GroupsController < ApplicationController
   before_filter :require_login
 
   def create
-    puts params.inspect
     friend_data = current_user.get_friends
     friend_json = JSON.parse friend_data
     group = Group.new(group_params)
+    group.convert_members(friend_json['data'])
     respond_to do |format|
-      if group.save_with_venmoids(friend_json['data'])
+      if group.save
         format.html { redirect_to '/' }
         format.js   {}
         # format.json { render json: group, status: :created, location: group }
       else
+        puts group.errors
         # format.json { render json: group.errors, status: :unprocessable_entity }
       end
     end
@@ -19,8 +20,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    puts "SFSDFS"
-    puts params.inspect
     params.require(:group).permit(:name, :members)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -8,21 +8,20 @@ class Group < ActiveRecord::Base
 
   #Custom Save
   #Need to change display names to venmo_ids
-  def save_with_venmoids(friends)
+  def convert_members(friends)
     if new_record?
+      members_as_venmoid = Array.new
       names = members.split(', ')
       names.reject! { |n| n.empty? }
-      (0..names.length-1).each do |i|
-        (0..friends.length-1).each do |j|
-          if friends[j]['display_name'] == name[i]
+      names.each do |n|
+        friends.each do |f|
+          if f['display_name'] == n
+            members_as_venmoid << f['id']
             break
-          end
-          #No match found. Should not happen
-          if j == friends.length
-            return false
           end
         end
       end
+      self.members = members_as_venmoid.to_json
       return true
     else
       return false
