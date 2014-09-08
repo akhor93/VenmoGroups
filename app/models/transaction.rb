ae45aae44af757abe39d6dd85e8cf14149e632b0
Andrew Khor <andrew.khow@venmo.com>
2014-09-08 15:32:44 -0400

    mobile optimization should be done

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -15,9 +15,9 @@ class Transaction < ActiveRecord::Base
     end
     transaction_ids = Array.new
     params[:members].each do |m|
-      # res_json = JSON.parse(RestClient.post('https://api.venmo.com/v1/payments', 'access_token' => user.access_token, 'user_id' => m, 'note' => params[:note], 'amount' => amount, 'audience' => 'friends'))
-      # payment = res_json['data']['payment']
-      # transaction_ids << payment['id']
+      res_json = JSON.parse(RestClient.post('https://api.venmo.com/v1/payments', 'access_token' => user.access_token, 'user_id' => m, 'note' => params[:note], 'amount' => amount, 'audience' => 'friends'))
+      payment = res_json['data']['payment']
+      transaction_ids << payment['id']
     end
     transaction_params = { :members => params[:members].to_json, :user => user, :amount => amount, :note => params[:note], :action => params[:action], :venmo_transaction_ids => transaction_ids.to_json }
     transaction = Transaction.create(transaction_params)
